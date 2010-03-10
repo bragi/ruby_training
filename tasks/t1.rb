c5254d80a9d8efbecb7d0f4713de3a725df2d733
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2010-03-10 14:50:19 +0100

    Simplified roman conversions

--- tasks/t1.rb
+++ tasks/t1.rb
@@ -1,26 +1,29 @@
 #!/usr/bin/env ruby
 
-module RomanConversion
-  def valid_arabic?
-    # TODO: implement properly
-    false
-  end
-  
+# Converts numbers from arabic to roman and the other way around
+#
+# Usage:
+#    t0.rb 4
+#    # prints: IV
+#    t0.rb IV
+#    # prints 4
+#    t0.rb
+#    # runs test suite
+
+class String
   def valid_roman?
-    # TODO: implement properly
-    false
+    # TODO: implement
   end
   
-  def to_i_with_roman_conversion
-    # TODO: implement properly
-    to_i_without_roman_conversion
+  def to_i_from_roman
+    # TODO: implement
   end
 end
 
-class String
-  include RomanConversion
-  alias to_i_without_roman_conversion to_i
-  alias to_i to_i_with_roman_conversion
+class Integer
+  def to_s_roman
+    # TODO: implement
+  end
 end
 
 class RomanNumerals
@@ -29,29 +32,17 @@ class RomanNumerals
   end
   
   def convert!
-    # TODO: implement properly
+    # TODO: implement
     exit(0) # Finish gracefully by default, change if appropriate
   end
 end
 
-#Make sure RomanNumbers#convert! ends the program with exit()
+# Make sure RomanNumbers#convert! ends the program with exit()
 RomanNumerals.new(ARGV).convert! unless ARGV.empty?
 
 require 'test/unit'
 
 class StringTest < Test::Unit::TestCase
-  def test_check_valid_arabic
-    assert "1".valid_arabic?
-    assert "1234567890".valid_arabic?
-    assert "44".valid_arabic?
-  end
-  
-  def test_check_invalid_arabic
-    assert !"aa".valid_arabic?
-    assert !"123,34".valid_arabic?
-    assert !"MMX".valid_arabic?
-  end
-
   def test_check_valid_roman
     assert "I".valid_roman?
     assert "IX".valid_roman?
@@ -71,24 +62,26 @@ class StringTest < Test::Unit::TestCase
     assert !"IIIII".valid_roman?
   end
   
-  def test_convert_valid_arabic
-    assert_equal 1234, "1234".to_i
-    assert_equal 1100101, "1100101".to_i
-    assert_equal 1100101, "1100101CXMMXC".to_i
-  end
-
-  def test_convert_non_number
-    assert_equal 0, "abc".to_i
-  end
-  
   def test_convert_valid_roman
-    assert_equal 1, "I".to_i
-    assert_equal 9, "IX".to_i
-    assert_equal 7, "VII".to_i
-    assert_equal 4, "IIII".to_i
-    assert_equal 4, "IV".to_i
-    assert_equal 1999, "MCMXCIX".to_i
-    assert_equal 1309, "MCCCIX".to_i
-    assert_equal 2012, "MMXII".to_i
+    assert_equal 1, "I".to_i_from_roman
+    assert_equal 9, "IX".to_i_from_roman
+    assert_equal 7, "VII".to_i_from_roman
+    assert_equal 4, "IIII".to_i_from_roman
+    assert_equal 4, "IV".to_i_from_roman
+    assert_equal 1999, "MCMXCIX".to_i_from_roman
+    assert_equal 1309, "MCCCIX".to_i_from_roman
+    assert_equal 2012, "MMXII".to_i_from_roman
+  end
+end
+
+class IntegerTest < Test::Unit::TestCase
+  def test_to_s_roman
+    assert_equal "I", 1.to_s_roman
+    assert_equal "IX", 9.to_s_roman
+    assert_equal "VII", 7.to_s_roman
+    assert_equal "IV", 4.to_s_roman
+    assert_equal "MCMXCIX", 1999.to_s_roman
+    assert_equal "MCCCIX", 1309.to_s_roman
+    assert_equal "MMXII", 2012.to_s_roman
   end
 end
