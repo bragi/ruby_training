b187ff6ea607f6d8375dd2712829ba23b6b4efe0
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2010-03-10 16:02:13 +0100

    Added example solution to T1

--- solutions/t1.rb
+++ solutions/t1.rb
@@ -1,17 +1,21 @@
 #!/usr/bin/env ruby
 
-module RomanConversion
-  def valid_arabic?
-    self =~ /^[0-9]+$/
-  end
-  
+# Converts numbers from arabic to roman and the other way around
+#
+# Usage:
+#    t0.rb 4
+#    # prints: IV
+#    t0.rb IV
+#    # prints 4
+#    t0.rb
+#    # runs test suite
+
+class String
   def valid_roman?
     self =~ /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,4})$/
   end
   
-  def to_i_with_roman_conversion
-    return to_i_without_roman_conversion unless valid_roman?
-
+  def to_i_from_roman
     values = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1}
     result = 0
     last_number = 0
@@ -26,10 +30,19 @@ module RomanConversion
   end
 end
 
-class String
-  include RomanConversion
-  alias to_i_without_roman_conversion to_i
-  alias to_i to_i_with_roman_conversion
+class Integer
+  def to_s_roman
+    values = {1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V", 6 => "VI", 7 => "VII", 8 => "VIII", 9 => "IX",
+      10 => "X", 20 => "XX", 30 => "XXX", 40 => "XL", 50 => "L", 60 => "LX", 70 => "LXX", 80 => "LXXX", 90 => "XC",
+      100 => "C", 200 => "CC", 300 => "CCC", 400 => "CD", 500 => "D", 600 => "DC", 700 => "DCC", 800 => "DCCC", 900 => "CM"
+    }
+    ones = self % 10
+    tens = (self - ones) % 100
+    hundreds = (self - tens - ones) % 1000
+    thousands = self / 1000
+    
+    (["M" * thousands] + [hundreds, tens, ones].map {|v| values[v]}).compact.join
+  end
 end
 
 class RomanNumerals
@@ -38,29 +51,17 @@ class RomanNumerals
   end
   
   def convert!
-    # TODO: implement properly
+    puts(@number.valid_roman? ? @number.to_i_from_roman : @number.to_i.to_s_roman)
     exit(0) # Finish gracefully by default, change if appropriate
   end
 end
 
-#Make sure RomanNumbers#convert! ends the program with exit()
+# Make sure RomanNumbers#convert! ends the program with exit()
 RomanNumerals.new(ARGV).convert! unless ARGV.empty?
 
 require 'test/unit'
 
 class StringTest < Test::Unit::TestCase
-  def test_check_valid_arabic
-    assert "1".valid_arabic?
-    assert "1234567890".valid_arabic?
-    assert "44".valid_arabic?
-  end
-  
-  def test_check_invalid_arabic
-    assert !"aa".valid_arabic?
-    assert !"123,34".valid_arabic?
-    assert !"MMX".valid_arabic?
-  end
-
   def test_check_valid_roman
     assert "I".valid_roman?
     assert "IX".valid_roman?
@@ -80,24 +81,26 @@ class StringTest < Test::Unit::TestCase
     assert !"IIIII".valid_roman?
   end
   
-  def test_convert_valid_arabic
-    assert_equal 1234, "1234".to_i
-    assert_equal 1100101, "1100101".to_i
-    assert_equal 1100101, "1100101CXMMXC".to_i
-  end
-
-  def test_convert_non_number
-    assert_equal 0, "abc".to_i
-  end
-  
   def test_convert_valid_roman
-    assert_equal 1, "I".to_i
-    assert_equal 9, "IX".to_i
-    assert_equal 7, "VII".to_i
-    assert_equal 4, "IIII".to_i
-    assert_equal 4, "IV".to_i
-    assert_equal 1999, "MCMXCIX".to_i
-    assert_equal 1309, "MCCCIX".to_i
-    assert_equal 2012, "MMXII".to_i
+    assert_equal 1, "I".to_i_from_roman
+    assert_equal 9, "IX".to_i_from_roman
+    assert_equal 7, "VII".to_i_from_roman
+    assert_equal 4, "IIII".to_i_from_roman
+    assert_equal 4, "IV".to_i_from_roman
+    assert_equal 1999, "MCMXCIX".to_i_from_roman
+    assert_equal 1309, "MCCCIX".to_i_from_roman
+    assert_equal 2012, "MMXII".to_i_from_roman
+  end
+end
+
+class IntegerTest < Test::Unit::TestCase
+  def test_to_s_roman
+    assert_equal "I", 1.to_s_roman
+    assert_equal "IX", 9.to_s_roman
+    assert_equal "VII", 7.to_s_roman
+    assert_equal "IV", 4.to_s_roman
+    assert_equal "MCMXCIX", 1999.to_s_roman
+    assert_equal "MCCCIX", 1309.to_s_roman
+    assert_equal "MMXII", 2012.to_s_roman
   end
 end
