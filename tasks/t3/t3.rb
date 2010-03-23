ab44c50167720d91d7c4e7c13f89deae0f2a5396
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2010-03-23 15:31:40 +0100

    Created t3 structure

---
+++ tasks/t3/t3.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# Install Sinatra framework:
+#     sudo gem install sinatra
+#
+# Documentation: 
+#  * http://www.sinatrarb.com/
+#  * http://railsapi.com/doc/sinatra-v1.0/
+#
+# Web application that runs on http://localhost:4567/
+
+require 'sinatra'
