9eca9a401915cfe3d4cb4d677482302effdc0ecb
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2010-04-11 14:18:40 +0200

    Implemented second example

--- solutions/t4/html_builder.rb
+++ solutions/t4/html_builder.rb
@@ -1,7 +1,16 @@
 # Allows to generate HTML using a nice Ruby syntax. Takes care of whitespace,
 # closing the tags properly and quoting attributes.
 class HtmlBuilder
-  def to_html
-    ""
+  def initialize
+    @to_html = ""
   end
+  
+  def to_html
+    @to_html
+  end
+  
+  def p
+    @to_html += "<p></p>"
+  end
+  
 end
--- solutions/t4/html_builder_test.rb
+++ solutions/t4/html_builder_test.rb
@@ -10,4 +10,9 @@ describe HtmlBuilder do
   it "has empty default representation" do
     @builder.to_html.should == ""
   end
+  
+  it "has representation of a single tag" do
+    @builder.p
+    @builder.to_html.should == "<p></p>"
+  end
 end  
