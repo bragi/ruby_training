02b886d0a908ec8f76efcb2b66aa5ff068d5eb98
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2010-04-11 14:46:58 +0200

    Working nesting, text capability, changed API to use to_s instead of to_html

--- solutions/t4/html_builder.rb
+++ solutions/t4/html_builder.rb
@@ -1,16 +1,33 @@
 # Allows to generate HTML using a nice Ruby syntax. Takes care of whitespace,
 # closing the tags properly and quoting attributes.
 class HtmlBuilder
-  def initialize
-    @to_html = ""
+  def initialize(name=nil)
+    @name = name
+    @tags = []
   end
   
-  def to_html
-    @to_html
+  def to_s
+    nested_tags = @tags.map {|t| t.to_s}
+    start = "<#{@name}>" if @name
+    finish = "</#{@name}>" if @name
+    [start, nested_tags, finish].compact.join
   end
   
-  def p
-    @to_html += "<p></p>"
+  def text(text)
+    @tags << text
   end
   
+  def tag(name)
+    tag = HtmlBuilder.new(name)
+    yield tag if block_given?
+    @tags << tag
+  end
+  
+  def p(&block)
+    tag("p", &block)
+  end
+
+  def html(&block)
+    tag("html", &block)
+  end
 end
--- solutions/t4/html_builder_test.rb
+++ solutions/t4/html_builder_test.rb
@@ -8,11 +8,31 @@ describe HtmlBuilder do
   end
   
   it "has empty default representation" do
-    @builder.to_html.should == ""
+    @builder.to_s.should == ""
   end
   
   it "has representation of a single tag" do
     @builder.p
-    @builder.to_html.should == "<p></p>"
+    @builder.to_s.should == "<p></p>"
+  end
+  
+  it "has representation of nested tags" do
+    @builder.html do |html|
+      html.p
+    end
+    @builder.to_s.should == "<html><p></p></html>"
+  end
+  
+  it "has text content" do
+    @builder.p do |p|
+      p.text "Some text"
+    end
+    @builder.to_s.should == "<p>Some text</p>"
+  end
+  
+  it "has representation for full size example" do
+    @builder.html do |html|
+      
+    end
   end
 end  
